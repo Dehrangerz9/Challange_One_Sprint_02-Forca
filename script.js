a053b248e545b5259bea837bb3b1c10b8ef75c6b
David Richard <davidrichardf.lima@gmail.com>
2022-10-01 11:06:03 -0300

    BIRBIRBI

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 //Seletores
-let palavras = ["ALURA", "ORACLE", "PARALELEPIPEDO", "CARAPICUIBA","OSASCO", "GENSHIN IMPACT", "BANANA", "ALFABETO", "MANJERICAO"];
-let forca = document.getElementById("forca");
-let canvas = forca.getContext("2d");
+let palavras = ["ALURA", "ORACLE", "PARALELEPIPEDO", "CARAPICUIBA","OSASCO", "GENSHIN IMPACT", "BANANA", "ALFABETO", "MANJERICAO", ""];
+let forca = document.querySelector('#forca').getContext("2d");
 let palavraSecreta = "";
+let letras = []
 
-
-//Funções - xxx
+//Funções - Do jogo
 function sortearPalavra(){
     let palavra = palavras[Math.floor(Math.random() * palavras.length)]
     palavraSecreta = palavra
+}
+
+function verificarLetra(key){
+    let estado = false
+    if(key >= 65 && letras.indexOf(key) || key <= 90 && letras.indexOf(key)){
+        letras.push(key)
+        console.log(key)
+        console.log(letras)
+        return estado  
+    }else{
+        letras.push(key)
+        console.log(key)
+        console.log(letras,"if true")
+        estado = true
+    }
 }
 
 function iniciarJogo(){
@@ -17,7 +31,18 @@ function iniciarJogo(){
     sortearPalavra()
     desenharCanvas()
     desenharLinha()
-    console.log(palavraSecreta)
+
+    document.onkeydown = (e) =>{
+        let letra = e.key.toUpperCase()
+
+        if(verificarLetra(letra) && palavraSecreta.includes(letra)){
+            for(let i = 0; i < palavraSecreta.length; i++){
+                if(palavraSecreta[i] === letra){
+                    
+                }
+            }
+        }
+    }
 }
 
 function adicionarPalavra(){
@@ -25,36 +50,40 @@ function adicionarPalavra(){
     document.getElementById('menu-cfg').style.display ='flex'
 }
 
+//Captar teclas digitadas
+//verificar se é uma letra
+//Saber se a palavra secreta inclui a letra digitada 
+// escrever a letra correta
+// se não, escrever letra incorreta
+//contar erros
 
 //Funções - Canvas
 function desenharCanvas(){
-    canvas.lineWidth = 8;
-    canvas.lineCap = "round";
-    canvas.lineJoin = "round";
-    canvas.fillStyle = "#eb0000";
-    canvas.strokeStyle = "#0A3871";
-
-    //manipulação tabuleiro
-    canvas.fillRect(0, 0, 1200, 800);
-    canvas.beginPath();
-    canvas.moveTo(900,500);
-    canvas.lineTo(650,500);
-    canvas.stroke();
-    canvas.closePath();
+    forca.lineWidth = 8;
+    forca.lineCap = "round";
+    forca.lineJoin = "round";
+    forca.fillStyle = "#eb0000";
+    forca.strokeStyle = "#0A3871";
+    forca.fillRect(0, 0, 1200, 800);
+    forca.beginPath();
+    forca.moveTo(50,500);
+    forca.lineTo(300,500);
+    forca.stroke();
+    forca.closePath();
 }
 
 function desenharLinha(){
-    canvas.lineWidth = 6;
-    canvas.lineCap = "round";
-    canvas.lineJoin = "round";
-    canvas.fillStyle = "#F3F5FC";
-    canvas.strokeStyle = "#0A3871";
+    forca.lineWidth = 6;
+    forca.lineCap = "round";
+    forca.lineJoin = "round";
+    forca.strokeStyle = "#000000";
 
-    let largura = 600/palavraSecreta.length
+    forca.beginPath()
+    let largura = 1000/palavraSecreta.length
     for(let i = 0; i < palavraSecreta.length; i++){
-        canvas.moveTo(500+(largura*i), 640)
-        canvas.lineTo(550+(largura*i), 640)
+        forca.moveTo(50+(largura*i), 640)
+        forca.lineTo(100+(largura*i), 640)
+        forca.stroke()
     }
-    canvas.stroke()
-    canvas.closePath()
+    forca.closePath()
 }
